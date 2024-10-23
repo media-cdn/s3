463784c08327ec6aa49907af88b403ec8609f28e
Your Name <you@example.com>
2024-10-23 17:30:23 +0700

    refactor: Refactor S3 client creation and usage in client and handler files

--- client/client.go
+++ client/client.go
@@ -1,14 +1,21 @@
 package client
 
 import (
+	"context"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func NewS3Client() *s3.Client {
+type S3Client struct {
+	pc *s3.PresignClient
+}
+
+func NewS3Client() *S3Client {
 	options := s3.Options{
 		BaseEndpoint: aws.String(os.Getenv("ENDPOINT")),
 		Region:       os.Getenv("REGION"),
@@ -20,5 +27,48 @@ func NewS3Client() *s3.Client {
 			),
 		),
 	}
-	return s3.New(options)
+	client := s3.New(options)
+	presignClient := s3.NewPresignClient(client)
+	return &S3Client{
+		pc: presignClient,
+	}
+}
+
+func (c *S3Client) GetObject(
+	ctx context.Context,
+	bucketName string,
+	path string,
+	header http.Header,
+) (*http.Response, error) {
+	if bucketName == "" {
+		return nil, ErrInvalidBucket
+	}
+	if strings.HasSuffix(path, "/") {
+		return nil, ErrInvalidPath
+	}
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(path),
+		Range:  aws.String(header.Get("Range")),
+	}
+	presignedGetRequest, err := c.pc.PresignGetObject(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodGet, presignedGetRequest.URL, nil)
+	if err != nil {
+		return nil, err
+	}
+	for key, values := range header {
+		for _, value := range values {
+			if key == "If-Modified-Since" || key == "If-None-Match" {
+				continue
+			}
+			req.Header.Add(key, value)
+		}
+	}
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Pragma", "no-cache")
+	httpClient := http.Client{}
+	return httpClient.Do(req)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.32.2
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.41
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.66.0
+	github.com/go-chi/chi/v5 v5.1.0
 )
 
 require (
@@ -18,5 +19,4 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.18.2 // indirect
 	github.com/aws/smithy-go v1.22.0 // indirect
-	github.com/go-chi/chi/v5 v5.1.0 // indirect
 )
