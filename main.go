732c53651a5af89a8e6509113c75673e05b712c2
Your Name <you@example.com>
2024-10-23 12:38:12 +0700

    feat: Add Dockerfile, go.mod, main.go, and update README.md
    Add Dockerfile for multi-stage build, update go.mod with AWS SDK dependencies,
    add main.go file for serving content, and update README.md for s3.

---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func main() {
+	// register the handler
+	s3Handler := newS3Handler()
+	http.HandleFunc("/", s3Handler.servingContent)
+
+	// start the server
+	port := ":8080"
+	fmt.Println("Server start at", port)
+	http.ListenAndServe(port, nil)
+}
+
+func newS3Client() *s3.Client {
+	return s3.New(s3.Options{
+		BaseEndpoint: aws.String(os.Getenv("ENDPOINT")),
+		Region:       os.Getenv("REGION"),
+		Credentials: aws.NewCredentialsCache(
+			credentials.NewStaticCredentialsProvider(
+				os.Getenv("KEY"),
+				os.Getenv("SECRET"),
+				"",
+			),
+		),
+	})
+}
+
+type s3Handler struct {
+	s3Client *s3.Client
+}
+
+func newS3Handler() *s3Handler {
+	return &s3Handler{
+		s3Client: newS3Client(),
+	}
+}
+
+// The servingContent function is the HTTP handler that takes a request, extracts the bucket name, object path from the parameters,
+// generates a signed URL for the private S3 object, and then response file content to client.
+func (h *s3Handler) servingContent(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimLeft(r.URL.Path, "/")
+	bucketName := strings.Split(path, "/")[0]
+	path = strings.Replace(path, bucketName+"/", "", 1)
+
+	// generate presigned URL
+	presignClient := s3.NewPresignClient(h.s3Client)
+	presignedGetRequest, err := presignClient.PresignGetObject(r.Context(), &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(path),
+		Range:  aws.String(r.Header.Get("Range")),
+	})
+	if err != nil {
+		log.Printf("failed to presign request: %v", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	// new http reqest
+	req, err := http.NewRequest(http.MethodGet, presignedGetRequest.URL, nil)
+	if err != nil {
+		log.Printf("failed to create request: %v", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	// set request headers
+	for key, values := range r.Header {
+		for _, value := range values {
+			// ignore header: If-Modified-Since, If-None-Match to prevent status 304
+			if key == "If-Modified-Since" || key == "If-None-Match" {
+				continue
+			}
+
+			req.Header.Add(key, value)
+		}
+	}
+	// set header no cache to prevent status 304
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Pragma", "no-cache")
+
+	httpClient := http.Client{}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		log.Printf("failed to get response: %v", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+	defer resp.Body.Close()
+
+	// check status code
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		log.Printf("failed to get response: %v", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	// set response headers
+	for key, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+
+	// check if the response contains partial content
+	// for streaming content like video, audio, etc.
+	if resp.Header.Get("Content-Range") != "" {
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", resp.ContentLength))
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.WriteHeader(http.StatusPartialContent)
+	}
+
+	// write response body to client
+	_ := io.Copy(w, resp.Body)
+}
